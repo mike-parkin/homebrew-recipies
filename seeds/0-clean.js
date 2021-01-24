e2ab9c7ec7447817f5cf1c5eb9ea859ce504e45c
Mike Parkin <michael.parkin1988@gmail.com>
2021-01-24 14:59:32 +1300

    update 0-clean seed file

--- seeds/0-clean.js
+++ seeds/0-clean.js
@@ -1,8 +1,10 @@
 exports.seed = function (knex, Promise) {
-  const empty = table =>
-    () => knex(table).del()
+  const empty = (table) => knex(table).del();
 
-  return empty('beers')()
+  return empty('beers')
+    .then(() => empty('grains'))
+    .then(() => empty('hops'))
+    .then(() => empty('yeasts'))
   // Chain calls to empty in
   // order as required, e.g.:
   // .then(empty('profiles'))
